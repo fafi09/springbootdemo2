2dfb4261ec4222be672919bc96423ea71235cf2d
fafi09 <fafi09@126.com>
2019-03-29 10:37:33 +0800

    v0.2添log4j

--- demo2/src/main/java/com/example/service/impl/User2TsServiceImpl.java
+++ demo2/src/main/java/com/example/service/impl/User2TsServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.service.impl;
 
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,12 @@ import com.example.service.User2TsService;
 import com.example.test.mapper.UserTestMapper;
 import com.example.test1.mapper.StudentMapper;
 
+
+
 @Service
 public class User2TsServiceImpl implements User2TsService {
+	
+	private static final Logger log = LoggerFactory.getLogger(User2TsServiceImpl.class);
 
 	@Autowired
 	private UserTestMapper userMapper;
@@ -21,10 +27,11 @@ public class User2TsServiceImpl implements User2TsService {
 	@Override
 	@Transactional
 	public int insert(User user) {
+		log.info("insert前");
 		userMapper.insert(user.getUserid(), user.getUsername(), user.getPassword(), user.getStatus());
 		int i = 2/0;
 		studentMapper.insert(user.getUserid(), user.getUsername(), 12, user.getStatus());
-		
+		log.info("insert后");
 		return 0;
 	}
 
